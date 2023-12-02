183f86d87c3084744960e7e40cfc4df1ae46ef8e
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2023-12-02 10:40:20 +0100

    save option selected, change the other 2 select, change prices when changing options

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ const tabPrices = [
   },
 ];
 
-
+/**
+ *
+ *
+ * @class controlSelect
+ */
 class controlSelect {
   #items
   #lesOptions
@@ -37,14 +41,27 @@ class controlSelect {
       }
       
       item.addEventListener('change', (e)=> this.#initPrice(item))
+      
+      
       this.#lesOptions = Array.from(item.querySelectorAll('option'))
+      console.log(this.#lesOptions);
 
       this.#initAttribut(this.#lesOptions)
+      
     })
+
+    var selectOption = localStorage.getItem('selectOption', selectOption)
+
+    if (selectOption) {
+      this.#ifselectOption(selectOption)
+    } else {
+      this.#ifselectOption(undefined)
+    }
+
+    
   }
 
   #initAttribut(lesElts) {
-    // console.log(lesElts);
 
     if (lesElts) {
       lesElts.forEach(function( elt, i) {
@@ -62,13 +79,83 @@ class controlSelect {
     }
   }
 
-  #initPrice(elt) {
-    console.log(elt);
+  #initPrice(item) {
+     /**
+    * @type {string}
+    */
+    var selectValue = item.options[item.selectedIndex].value
+    /**
+     * @type {object}
+     */
+    var target = item
+
+    localStorage.setItem('selectOption', selectValue)
+
+    if (selectValue === 'option1') {
+      this.#updateTarifs(['3,00', '3,00', '13,00']) 
+    } else if(selectValue === 'option2') {
+      this.#updateTarifs(['5,76', '6,32', '14,90']) 
+    } else {
+      this.#updateTarifs(['7,20', '7,90', '14,90']) 
+    }
+
+
+    var others = this.#items.filter( item => {
+      return item !== target
+    })
+
+    for (const key in others) {
+        others[key].value = target.value
+    }
+
   }
+
+  #updateTarifs(tabTarifs) {
+
+    for (let index = 0; index < this.#items.length; index++) {
+      const element = this.#items[index];
+
+      var eltPrice = element.parentElement.nextElementSibling;
+      var price = eltPrice.firstElementChild;
+  
+      price.textContent = tabTarifs[index] + ' €'
+    }
+    
+  }
+
+  #ifselectOption(value) {
+
+
+    for (const key in this.#items) {
+      if (value) {
+        this.#items[key].value = value
+
+        if (value === 'option1') {
+          this.#updateTarifs(['3,00', '3,00', '13,00']) 
+        } else if(value === 'option2') {
+          this.#updateTarifs(['5,76', '6,32', '14,90']) 
+        }else {
+          this.#updateTarifs(['7,20', '7,90', '14,90'])
+        }
+      } else {
+        this.#items[key].value = 'option1'
+        this.#updateTarifs(['3,00', '3,00', '13,00']) 
+      }
+    }
+  }
+
+  // #foundEltPrice(elt) {
+  //   var priceSelectDefault = elt.options[elt.selectedIndex].dataset.price;
+  //   var eltPrice = elt.parentElement.nextElementSibling;
+  //   var price = eltPrice.firstElementChild;
+
+  //   price.innerHTML = priceSelectDefault
+  // }
 }
 
 
 addEventListener('load', function () {
+  /** @type {*} */
   const items = Array.from(document.querySelectorAll('select[data-select]'))
   
   // items.forEach( item => {
